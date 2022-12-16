b24885e22a78025d726aed2da87da2db873076c4
DevExpressExampleBot <fakeEmail>
2022-12-16 21:11:45 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/CustomBarElements.vb
+++ VB/WpfApplication1/CustomBarElements.vb
@@ -1,38 +1,30 @@
-﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Bars
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
 Imports System.Windows.Media
 
 Namespace WpfApplication1
-    Public NotInheritable Class CustomBarElementsRegistrator
 
-        Private Sub New()
-        End Sub
+    Public Module CustomBarElementsRegistrator
 
-        Public Shared Sub Register()
-            BarItemLinkCreator.Default.RegisterObject(GetType(MyBarButtonItem), GetType(MyBarButtonItemLink), Function(link) New MyBarButtonItemLink())
-            BarItemLinkControlCreator.Default.RegisterObject(GetType(MyBarButtonItemLink), GetType(MyBarButtonItemLinkControl), Function(link) New MyBarButtonItemLinkControl(CType(link, MyBarButtonItemLink)))
+        Public Sub Register()
+            Call BarItemLinkCreator.Default.RegisterObject(GetType(MyBarButtonItem), GetType(MyBarButtonItemLink), Function(link) New MyBarButtonItemLink())
+            Call BarItemLinkControlCreator.Default.RegisterObject(GetType(MyBarButtonItemLink), GetType(MyBarButtonItemLinkControl), Function(link) New MyBarButtonItemLinkControl(CType(link, MyBarButtonItemLink)))
         End Sub
-    End Class
+    End Module
 
     Public Class MyBarButtonItemLinkControl
         Inherits BarButtonItemLinkControl
 
         Public Sub New()
         End Sub
+
         Public Sub New(ByVal link As MyBarButtonItemLink)
             MyBase.New(link)
-
         End Sub
 
         Public Overrides Sub OnApplyTemplate()
             MyBase.OnApplyTemplate()
-
             'demo test
             Me.LayoutPanel.Background = Brushes.Red
         End Sub
@@ -43,7 +35,6 @@ Namespace WpfApplication1
 
     End Class
 
-
     Public Class MyBarButtonItem
         Inherits BarButtonItem
 
@@ -51,5 +42,4 @@ Namespace WpfApplication1
             Return GetType(MyBarButtonItemLink)
         End Function
     End Class
-
 End Namespace
